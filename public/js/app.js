9ef4e384f17a7f3458f38049233be85613b4b23a
darkenvy <darkenvy6@gmail.com>
2016-07-27 08:38:31 -0700

    Added ui.router

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,22 @@
-var app = angular.module('UserApp', []);
+var app = angular.module('UserApp', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      .state('plane', {
+        url: '/',
+        controller: 'PlaneCtrl',
+        templateUrl: 'app/planes.html'
+      })
+      .state('404', {
+        url: '/404',
+        controller: '',
+        template: '<h1>404</h1>'
+      })
+
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/404');
+}])
 
 app.controller('PlaneCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.plane = {
@@ -15,16 +33,22 @@ app.controller('PlaneCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.delete = function(id, idx) {
     var route = '/api/airplanes/' + id
-    $http.delete(route).then(function success(res) {
-      $scope.planes.splice(idx, 1)
-      console.log(res);
-    }, function error(res) {
-      console.log(res);
-    })
+    $http.delete(route)
+         .then(function success(res) {
+            $scope.planes.splice(idx, 1)
+            console.log(res);
+          }, function error(res) {
+            console.log(res);
+          })
   }
 
   $scope.formSubmit = function() {
-
+    $http.post('/api/airplanes', $scope.plane)
+         .then(function success(res) {
+            $scope.planes.push(res.data)
+         }, function error(err) {
+          console.log(err);
+         });
   }
 
 }]);
